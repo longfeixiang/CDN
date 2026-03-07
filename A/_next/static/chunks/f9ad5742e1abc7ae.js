91329114361342cae36a7637c78c256e026140b7
Longfeixiang <superpangzi@outlook.com>
2026-03-07 14:09:41 +0800

    feat: update static assets for deployment 2026-03-07 14:09:41

---
+++ A/_next/static/chunks/f9ad5742e1abc7ae.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,91005,e=>{"use strict";var s=e.i(16774);class c extends s.WebPlugin{async show(e){}async hide(e){}}e.s(["SplashScreenWeb",()=>c])}]);
